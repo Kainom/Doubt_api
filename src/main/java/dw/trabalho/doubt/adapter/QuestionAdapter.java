98ba341a16f8f205ce232baa1756d23f1b07da4e
Kainom <eleuteriokaina@gmail.com>
2024-11-22 16:44:41 -0300

    Diretor added

--- src/main/java/dw/trabalho/doubt/adapter/QuestionAdapter.java
+++ src/main/java/dw/trabalho/doubt/adapter/QuestionAdapter.java
@@ -1,20 +1,14 @@
 package dw.trabalho.doubt.adapter;
 
 import dw.trabalho.doubt.control.dto.QuestionDto;
+import dw.trabalho.doubt.diretor.QuestionDtoDIrector;
 import dw.trabalho.doubt.model.Question;
 
 public class QuestionAdapter implements IQuenstionAdapter {
     @Override
     public QuestionDto tDto(Question question) {
-        return new QuestionDto().builder()
-                .answered(question.isAnswered())
-                .title(question.getTitle())
-                .description(question.getDescription())
-                .tags(question.getTags())
-                .timestamp(question.getTimestamp())
-                .questionId(question.getQuestionId())
-                .build();
 
+        return new QuestionDtoDIrector().getDefaultQuestionDto(question);
     }
 
     @Override
